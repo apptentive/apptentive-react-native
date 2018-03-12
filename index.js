d64019086280f7bc73afebe4aadc8989713ebd97
Alex Lementuev <a.lementuev@gmail.com>
2018-03-12 15:13:38 -0700

    Added a generated library project
    Generated by https://github.com/frostney/react-native-create-library

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNApptentiveModule } = NativeModules;
+
+export default RNApptentiveModule;
---
+++ ios/RNApptentiveModule.m
@@ -0,0 +1,13 @@
+
+#import "RNApptentiveModule.h"
+
+@implementation RNApptentiveModule
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
