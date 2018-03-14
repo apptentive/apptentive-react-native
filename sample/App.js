5fd6b7b89408afaad2e43cf46287d87a1b98fe4a
Alex Lementuev <a.lementuev@gmail.com>
2018-03-14 16:14:42 -0700

    Fixed javascript bindings

--- sample/App.js
+++ sample/App.js
@@ -12,15 +12,32 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import { Apptentive, ApptentiveConfiguration } from 'apptentive-react-native';
+
+const credentials = Platform.select({
+  ios: {
+    apptentiveKey: '___YOUR_IOS_APPTENTIVE_KEY___',
+    apptentiveSignature: '___YOUR_IOS_APPTENTIVE_SIGNATURE___',
+    jwtSigning: 'f4347328dccc33599bb5fbe8adcdbe88'
+  },
+  android: {
+    apptentiveKey: '___YOUR_ANDROID_APPTENTIVE_KEY___',
+    apptentiveSignature: '___YOUR_ANDROID_APPTENTIVE_SIGNATURE___c',
+    jwtSigning: 'dd4cf3d158e2ad5bc285733ef191d235'
+  }
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    const configuration = new ApptentiveConfiguration(
+      credentials.apptentiveKey,
+      credentials.apptentiveSignature
+    );
+    configuration.logLevel = 'verbose';
+    Apptentive.register(configuration);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -30,9 +47,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.instructions}>TODO: implement me</Text>
       </View>
     );
   }
