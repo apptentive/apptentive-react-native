7f85d9dedb6835269c00240b4a30c152f91bca1a
Alex Lementuev <a.lementuev@gmail.com>
2018-03-21 11:02:32 -0700

    Build fix

--- sample/android/app/src/main/java/com/apptentive/reactnative/sample/MainApplication.java
+++ sample/android/app/src/main/java/com/apptentive/reactnative/sample/MainApplication.java
@@ -2,13 +2,12 @@ package com.apptentive.reactnative.sample;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
 import com.apptentive.android.sdk.reactlibrary.RNApptentiveModulePackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.sample.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;
