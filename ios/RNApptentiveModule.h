7d899f2fe3145ccf4278511ee5b62f251a30e89d
Alex Lementuev <a.lementuev@gmail.com>
2018-03-22 14:17:18 -0700

    [WIP]

--- ios/RNApptentiveModule.h
+++ ios/RNApptentiveModule.h
@@ -1,11 +1,10 @@
 
 #if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
 #else
-#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #endif
 
-@interface RNApptentiveModule : NSObject <RCTBridgeModule>
+@interface RNApptentiveModule : RCTEventEmitter <RCTBridgeModule>
 
 @end
-  
