c23683e40d401a4613faa6326b8ff8010b54d01a
Alex Lementuev <a.lementuev@gmail.com>
2018-03-29 11:19:31 -0700

    Added missing doc comments

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ export class Apptentive {
   }
 
   /**
-   * Presents Message Center modally from the specified view controller.
+   * Presents Message Center.
    * @param customData An optional dictionary of key/value pairs to be associated with the event.
    * @return Promise with success boolean or error.
    */
@@ -58,10 +58,18 @@ export class Apptentive {
 
   // TODO: unread message count
 
+  /**
+   * Checks if Message Center will be displayed when `presentMessageCenter` is called.
+   * @return Promise with boolean flag or error.
+   */
   static canShowMessageCenter() {
     return RNApptentiveModule.canShowMessageCenter();
   }
 
+  /**
+   * Checks whether the given event will cause an Interaction to be shown.
+   * @return Promise with boolean flag or error.
+   */
   static canShowInteraction(event) {
     return RNApptentiveModule.canShowInteraction(event);
   }
@@ -70,6 +78,7 @@ export class Apptentive {
    * Shows interaction UI, if applicable, related to a given event, and attaches the specified custom data to the event.
    * @param event A string representing the name of the event.
    * @param customData An optional dictionary of key/value pairs to be associated with the event.
+   * @return Promise with boolean flag or error.
    */
   static engage(event, customData = null) {
     return RNApptentiveModule.engage(event, customData);
@@ -77,22 +86,44 @@ export class Apptentive {
 
   // TODO: extended data
 
+  /**
+   * @return Promise with person name or error.
+   */
   static getPersonName() {
     return RNApptentiveModule.getPersonName();
   }
 
+  /**
+   * Sets person name.
+   * @param value New person name.
+   * @return Promise with boolean flag or error.
+   */
   static setPersonName(value) {
     return RNApptentiveModule.setPersonName(value);
   }
 
+  /**
+   * @return Promise with person email or error.
+   */
   static getPersonEmail() {
     return RNApptentiveModule.getPersonEmail();
   }
 
+  /**
+   * Sets person email.
+   * @param value New person email.
+   * @return Promise with boolean flag or error.
+   */
   static setPersonEmail(value) {
     return RNApptentiveModule.setPersonEmail(value);
   }
 
+  /**
+   * Adds custom data associated with the current person.
+   * @param key String key for the data.
+   * @param value Value for the data (must be string, number or boolean)
+   * @return Promise with boolean flag or error.
+   */
   static addCustomPersonData(key, value) {
     if (value !== null && value !== undefined) {
       const type = typeof value;
@@ -107,10 +138,21 @@ export class Apptentive {
     return Promise.reject("Your value should be either a string, number or bool")
   }
 
+  /**
+   * Removes custom data associated with the current person.
+   * @param key String key for the data.
+   * @return Promise with boolean flag or error.
+   */
   static removeCustomPersonData(key) {
     return RNApptentiveModule.removeCustomPersonData(key);
   }
 
+  /**
+   * Adds custom data associated with the current device.
+   * @param key String key for the data.
+   * @param value Value for the data (must be string, number or boolean)
+   * @return Promise with boolean flag or error.
+   */
   static addCustomDeviceData(key, value) {
     if (value !== null && value !== undefined) {
       const type = typeof value;
@@ -125,30 +167,60 @@ export class Apptentive {
     return Promise.reject("Your value should be either a string, number or bool")
   }
 
+  /**
+   * Removes custom data associated with the current device.
+   * @param key String key for the data.
+   * @return Promise with boolean flag or error.
+   */
   static removeCustomDeviceData(key) {
     return RNApptentiveModule.removeCustomDeviceData(key);
   }
 
-  static logIn(jwt) {
-    return RNApptentiveModule.logIn(jwt);
+  /**
+   * Logs the specified user in, using the value of the proof parameter to
+   * ensure that the login attempt is authorized.
+   * @param token An authorization token.
+   * @return Promise with boolean flag or error.
+   */
+  static logIn(token) {
+    return RNApptentiveModule.logIn(token);
   }
 
+  /**
+   * Ends the current user session. The user session will be persisted in a logged-out state
+   * so that it can be resumed using the logIn method.
+   * @return Promise with boolean flag or error.
+   */
   static logOut() {
     return RNApptentiveModule.logOut();
   }
 
+  /**
+   * @return Current callback for the unread message count change in the Message Center.
+   */
   static get onUnreadMessageChange() {
     return _onUnreadMessageChange;
   }
 
+  /**
+   * Sets current callback for the unread message count change in the Message Center.
+   * @param value Callback function with a single integer parameter.
+   */
   static set onUnreadMessageChange(value) {
     _onUnreadMessageChange = value;
   }
 
+  /**
+   * @return Current callback for the authentication failures.
+   */
   static get onAuthenticationFailed() {
     return _onAuthenticationFailed;
   }
 
+  /**
+   * Sets current callback for the authentication failures.
+   * @param value Callback function with a single string parameter.
+   */
   static set onAuthenticationFailed(value) {
     _onAuthenticationFailed = value;
   }
