8ea959db6dd60f5d6e3d7f44b21880e36aa3651c
Matthew Callis <matthew.callis@gmail.com>
2018-03-29 16:32:09 -0700

    Run through ESLint to fix linting issues, correct package format

--- js/platform-specific.ios.js
+++ js/platform-specific.ios.js
@@ -1,11 +1,11 @@
-import { NativeEventEmitter } from 'react-native'
+import { NativeEventEmitter } from 'react-native';
 
 export class ApptentivePlatformSpecific {
   static createApptentiveEventEmitter(nativeModule) {
-    return new NativeEventEmitter(nativeModule)
+    return new NativeEventEmitter(nativeModule);
   }
 }
 
 module.exports = {
-  ApptentivePlatformSpecific
-}
+  ApptentivePlatformSpecific,
+};
