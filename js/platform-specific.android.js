c6c21e935a51cfb68ae2c56390e99932a75cbed4
Alex Lementuev <a.lementuev@gmail.com>
2018-10-29 08:27:36 -0700

    Cast custom data numbers to strings to avoid precision loss

--- js/platform-specific.android.js
+++ js/platform-specific.android.js
@@ -4,6 +4,13 @@ export class ApptentivePlatformSpecific {
   static createApptentiveEventEmitter(nativeModule) {
     return DeviceEventEmitter;
   }
+
+  /** We need to serialize number to a string in order to retain the precision -
+   * Android library would automatically cast values to Integer.
+   */
+  static exportNumber(value) {
+    return value.toString()
+  }
 }
 
 module.exports = {
