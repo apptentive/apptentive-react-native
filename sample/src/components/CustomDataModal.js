30819402b49c45102e5861a74bb4836c585a9796
Matthew Callis <matthew.callis@gmail.com>
2019-11-11 16:23:23 -0800

    Revert to old sample code for history

--- sample/src/components/CustomDataModal.js
+++ sample/src/components/CustomDataModal.js
@@ -1,163 +1,112 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-max-depth */
-/* eslint-disable react/no-set-state */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable react/require-optimization */
-import React from 'react';
-import { View, Modal, Button, TextInput, Picker } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Modal, Button, TextInput, Picker } from 'react-native';
 import { Apptentive } from 'apptentive-react-native';
 
-const styles = {
+export default class CustomDataModal extends Component {
+  constructor() {
+    super()
+    this.state = { key: '', value: '', type: 'string'}
+  }
+
+  render() {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        onRequestClose={() => { this.props.closeHandler() }}
+        >
+        <View style={styles.container} >
+          <Button title="Done" onPress={() => {
+              this.props.closeHandler()
+            }}
+          />
+          <Picker
+            selectedValue={this.state.type}
+            onValueChange={(itemValue, itemIndex) => this.setState({type: itemValue})}>
+            <Picker.Item label="String" value="string" />
+            <Picker.Item label="Boolean" value="boolean" />
+            <Picker.Item label="Number" value="number" />
+          </Picker>
+          <View style={styles.dataContainer}>
+            <View style={styles.textInputButtonRow}>
+              <TextInput
+                style={styles.borderedTextInput}
+                placeholder={'Custom data key'}
+                value={this.state.key}
+                onChangeText={(text) => this.setState({key: text})}
+              />
+              <Button
+                style={styles.button}
+                onPress={() => {
+                  if (this.props.mode == 'person') {
+                    Apptentive.removeCustomPersonData(this.state.key)
+                  } else if (this.props.mode == 'device') {
+                    Apptentive.removeCustomDeviceData(this.state.key)
+                  }
+                }
+              }
+              title="Remove"
+            />
+            </View>
+            <View
+              style={styles.textInputButtonRow}>
+              <TextInput
+                style={styles.borderedTextInput}
+                placeholder={`Custom data ${this.state.type}`}
+                value={this.state.value}
+                onChangeText={(text) => this.setState({value: text})}
+              />
+              <Button
+                style={styles.button}
+                onPress={() => {
+                  if (this.props.mode == 'person') {
+                    const value = this._getTypedValue(this.state.value, this.state.type)
+                    Apptentive.addCustomPersonData(this.state.key, value)
+                  } else if (this.props.mode == 'device') {
+                    const value = this._getTypedValue(this.state.value, this.state.type)
+                    Apptentive.addCustomDeviceData(this.state.key, value)
+                  }
+                }}
+                title="Add"/>
+            </View>
+          </View>
+        </View>
+    </Modal>);
+  }
+
+  _getTypedValue(value, type) {
+    switch (type) {
+      case 'number':
+        return Number.parseFloat(value)
+      case 'boolean':
+        return value == 'true'
+      default:
+        return value
+    }
+  }
+}
+
+styles = {
   container: {
     flex: 1,
     marginTop: 22,
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
   dataContainer: {
     flexDirection: 'column',
-    height: 80,
+    height: 80
   },
   textInputButtonRow: {
     flex: 1,
     flexDirection: 'row',
-    height: 40,
+    height: 40
   },
   borderedTextInput: {
     flex: 1,
     borderColor: 'gray',
     borderWidth: 1,
-    height: 40,
+    height: 40
   },
   button: {
-  },
-};
-
-export default class CustomDataModal extends React.Component {
-  static getTypedValue(value, type) {
-    switch (type) {
-      case 'number':
-        return Number.parseFloat(value);
-      case 'boolean':
-        return value === 'true';
-      default:
-        return value;
-    }
-  }
-
-  constructor() {
-    super();
-    this.state = {
-      key: '',
-      value: '',
-      type: 'string',
-    };
-
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-  }
-
-  handleAdd() {
-    const { mode } = this.props;
-    const { key, type, value } = this.state;
-    if (mode === 'person') {
-      Apptentive.addCustomPersonData(key, CustomDataModal.getTypedValue(value, type));
-    } else if (mode === 'device') {
-      Apptentive.addCustomDeviceData(key, CustomDataModal.getTypedValue(value, type));
-    }
-  }
-
-  handleRemove() {
-    const { mode } = this.props;
-    const { key } = this.state;
-    if (mode === 'person') {
-      Apptentive.removeCustomPersonData(key);
-    } else if (mode === 'device') {
-      Apptentive.removeCustomDeviceData(key);
-    }
-  }
-
-  render() {
-    const { closeHandler } = this.props;
-    const { key, type, value } = this.state;
-    const placeholder = `Custom data ${type}`;
-    return (
-      <Modal
-        accessibilityLabel="custom-data-modal"
-        animationType="slide"
-        onRequestClose={closeHandler}
-        testID="custom-data-modal"
-        transparent={false}
-      >
-        <View
-          accessibilityLabel="custom-data-view"
-          style={styles.container}
-          testID="custom-data-view"
-        >
-          <Button
-            accessibilityLabel="button-done"
-            onPress={closeHandler}
-            testID="button-done"
-            title="Done"
-          />
-          <Picker
-            accessibilityLabel="picker-type"
-            onValueChange={itemValue => this.setState({ type: itemValue })}
-            selectedValue={type}
-            testID="picker-type"
-          >
-            <Picker.Item label="String" value="string" />
-            <Picker.Item label="Boolean" value="boolean" />
-            <Picker.Item label="Number" value="number" />
-          </Picker>
-          <View
-            accessibilityLabel="view-button-rows"
-            style={styles.dataContainer}
-            testID="view-button-rows"
-          >
-            <View
-              accessibilityLabel="view-button-row-add"
-              style={styles.textInputButtonRow}
-              testID="view-button-row-add"
-            >
-              <TextInput
-                accessibilityLabel="text-input-key"
-                onChangeText={text => this.setState({ key: text })}
-                placeholder="Custom data key"
-                style={styles.borderedTextInput}
-                testID="text-input-key"
-                value={key}
-              />
-              <Button
-                accessibilityLabel="button-remove"
-                onPress={this.handleRemove}
-                style={styles.button}
-                testID="button-remove"
-                title="Remove"
-              />
-            </View>
-            <View
-              accessibilityLabel="view-button-row-remove"
-              style={styles.textInputButtonRow}
-              testID="view-button-row-remove"
-            >
-              <TextInput
-                accessibilityLabel="text-input-value"
-                onChangeText={text => this.setState({ value: text })}
-                placeholder={placeholder}
-                style={styles.borderedTextInput}
-                testID="text-input-value"
-                value={value}
-              />
-              <Button
-                accessibilityLabel="button-add"
-                onPress={this.handleAdd}
-                style={styles.button}
-                testID="button-add"
-                title="Add"
-              />
-            </View>
-          </View>
-        </View>
-      </Modal>);
   }
 }
