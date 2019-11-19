7e1aca4012a1c4daa334850f6d0d66b53300fe5f
Matthew Callis <matthew.callis@gmail.com>
2019-11-19 13:48:42 -0800

    Remove debug codepath

--- sample61/ios/sample61/AppDelegate.m
+++ sample61/ios/sample61/AppDelegate.m
@@ -32,11 +32,7 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
 }
 
 @end
