540c789ac394fd8ae091d9944d2bc730812417a8
Matthew Callis <matthew.callis@gmail.com>
2019-12-04 11:18:09 -0800

    Stubs for Network requests

--- sample61/ios/sample61UITests/sample61UITests.swift
+++ sample61/ios/sample61UITests/sample61UITests.swift
@@ -1,7 +1,7 @@
 import XCTest
+import OHHTTPStubs
 
 class sample61UITests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -16,7 +16,7 @@ class sample61UITests: XCTestCase {
     }
 
     // NOTE: This test assumes there is a valid Apptentive key / signature.
-    func testExample() {
+    func testNoInteractionForEvent() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
@@ -28,6 +28,51 @@ class sample61UITests: XCTestCase {
         XCTAssert(app.alerts.element.staticTexts["Interaction \"fake\" was not engaged"].exists)
     }
 
+    func testInteractionForEvent() {
+      stub(condition: isHost("api.apptentive.com") && isPath("/conversations")) { request in
+        return OHHTTPStubsResponse(
+          fileAtPath: OHPathForFile("conversations.json", type(of: self))!,
+          statusCode: 201,
+          headers: ["Content-Type":"application/json"]
+        )
+      }
+
+      stub(condition: isHost("api.apptentive.com") && isPath("/conversations/5dd709938bc3a004a50f410e/configuration")) { request in
+        return OHHTTPStubsResponse(
+          fileAtPath: OHPathForFile("conversations-5dd709938bc3a004a50f410e-configuration.json", type(of: self))!,
+          statusCode: 200,
+          headers: ["Content-Type":"application/json"]
+        )
+      }
+
+      stub(condition: isHost("api.apptentive.com") && isPath("/conversations/5dd709938bc3a004a50f410e/interactions")) { request in
+        return OHHTTPStubsResponse(
+          fileAtPath: OHPathForFile("conversations-5dd709938bc3a004a50f410e-interactions.json", type(of: self))!,
+          statusCode: 200,
+          headers: ["Content-Type":"application/json"]
+        )
+      }
+
+      stub(condition: isHost("api.apptentive.com") && isPath("/conversations/5dd709938bc3a004a50f410e/events")) { request in
+        return OHHTTPStubsResponse(
+          fileAtPath: OHPathForFile("conversations-5dd709938bc3a004a50f410e-events.json", type(of: self))!,
+          statusCode: 201,
+          headers: ["Content-Type":"application/json"]
+        )
+      }
+
+      // UI tests must launch the application that they test.
+      let app = XCUIApplication()
+      app.launch()
+      XCTAssert(!app.alerts.element.staticTexts["Please, provide Apptentive Key"].exists)
+      app.textFields["input-event-name"].tap()
+      app.typeText("note")
+      app.buttons["button-engage"].tap()
+      XCTAssertEqual(app.alerts.element.label, "Interaction")
+      XCTAssert(app.alerts.element.staticTexts["Interaction \"fake\" was not engaged"].exists)
+    }
+
+  
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
