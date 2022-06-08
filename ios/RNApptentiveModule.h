c43777bffc801f441c0274e9c2af97c130b1a24c
Luke Taylor <lmcjt@outlook.com>
2022-06-08 11:47:46 +0100

    remove conditional imports

--- ios/RNApptentiveModule.h
+++ ios/RNApptentiveModule.h
@@ -1,9 +1,4 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTEventEmitter.h"
-#else
 #import <React/RCTEventEmitter.h>
-#endif
 
 @interface RNApptentiveModule : RCTEventEmitter <RCTBridgeModule>
 
