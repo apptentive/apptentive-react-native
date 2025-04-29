f26a9b889b6f3f16a0c65274356c51804208c296
Frank Schmitt <frank.schmitt@alchemer.com>
2025-04-29 14:55:54 -0700

    Allow app to pass API base URL (for testing on staging)

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,7 @@ export class ApptentiveConfiguration {
   shouldInheritAppTheme: boolean;
   ratingInteractionThrottleLength: number;
   customAppStoreURL: string | null;
+  apiBaseURL: string | null;
 
   constructor(key: string, signature: string) {
     this.apptentiveKey = key;
@@ -42,6 +43,7 @@ export class ApptentiveConfiguration {
     this.shouldInheritAppTheme = true;
     this.ratingInteractionThrottleLength = 604800000;
     this.customAppStoreURL = null;
+    this.apiBaseURL = null;
   }
 }
 
